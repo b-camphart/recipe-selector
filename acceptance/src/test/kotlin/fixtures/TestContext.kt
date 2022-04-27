8ef02aee450a5ec39820ed877f64f4d33e91c7e1
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2022-04-26 23:52:41 -0700

    :test_tube: Ensure messages are displayed

--- acceptance/src/test/kotlin/fixtures/TestContext.kt
+++ acceptance/src/test/kotlin/fixtures/TestContext.kt
@@ -1,5 +1,14 @@
 package fixtures
 
-class TestContext {
+import MessagePresenter
+
+class TestContext : MessagePresenter {
+
+    private val _messages = mutableListOf<String>()
+    val messages: List<String> = _messages
+
+    override fun noRecipes() {
+        _messages.add("NO RECIPES")
+    }
 
 }
--- acceptance/src/test/kotlin/steps/Steps.kt
+++ acceptance/src/test/kotlin/steps/Steps.kt
@@ -1,10 +1,13 @@
 package steps
 
 import io.cucumber.java.en.Then
+import org.junit.jupiter.api.Assertions.assertTrue
 
 class Steps {
 
-    @Then("pass")
-    fun pass() = Unit
+    @Then("a {string} message should be displayed")
+    fun a_message_should_be_displayed(message: String) {
+        assertTrue(context.messages.contains(message))
+    }
 
 }
